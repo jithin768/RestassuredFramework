b0119c59f3f30f3c7801348281a66f6dae6b29b9
Jithinmathew <jithinmathew65@gmail.com>
2023-08-09 00:18:56 -0300

    Setting random datas via Data Faker

--- src/test/java/airlines/Payloads.java
+++ src/test/java/airlines/Payloads.java
@@ -1,5 +1,8 @@
 package airlines;
 
+import net.datafaker.Faker;
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -18,6 +21,22 @@ public class Payloads {
         payload.put("head_quaters",head_quaters);
         payload.put("website",website);
         payload.put("established",established);
+
+        return payload;
+    }
+
+    public static Map<String, Object> getPayloadsfromMap(){
+
+        Map<String,Object> payload=new HashMap<>();
+        Faker faker=new Faker();
+        payload.put("id",faker.number().digits(10));
+        payload.put("name",faker.name().firstName());
+        payload.put("country",faker.address().country());
+        payload.put("logo", RandomStringUtils.randomAlphabetic(25));
+        payload.put("slogan",RandomStringUtils.randomAlphabetic(25));
+        payload.put("head_quaters",faker.address().city());
+        payload.put("website","http://"+RandomStringUtils.randomAlphabetic(10)+".com");
+        payload.put("established",faker.number().numberBetween(1900,2020));
 
         return payload;
     }
