92b5cd65bbf0c2a1890d14843920823cad586967
jithin768 <jithinmathew65@gmail.com>
2023-08-12 17:39:11 -0300

    Update StudentApp.java
    Learning Git Pull on the current branch

--- src/main/java/JavaPrograms/StudentApp.java
+++ src/main/java/JavaPrograms/StudentApp.java
@@ -14,6 +14,7 @@ public class StudentApp {
         System.out.println(student.getCity());
         student.setState("Kerala");
         System.out.println(student.getState());
+        System.out.println("Test Git Pull");
 
     }
 }
